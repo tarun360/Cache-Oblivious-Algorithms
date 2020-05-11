d66a096610065cd90f6b9aa8fb1bd667a3e1de5f
Tarun Gupta <tarungupta360@gmail.com>
2020-05-11 20:32:23 +0530

    changed funnelsort

--- funnelsort.cpp
+++ funnelsort.cpp
@@ -2,32 +2,30 @@
 using namespace std;
 #define SIZE 16
 #define CACHELINE_SIZE 64
-
-typedef int (*cmp_t)(const void *, const void *);
-
 int nums[SIZE];
 
-int intcmp(const void *p_a, const void *p_b)
+typedef int (*cmp_t)(const void *, const void *);
+int intcmp(const void *ptr_a, const void *ptr_b)
 {
-	return *(int*)p_a - *(int*)p_b;
+	return *(int*)ptr_a - *(int*)ptr_b;
 }
 
-void generate(int *data, int size)
+void GenerateRandomNumers(int *nums, int size)
 {
 	srand(time(0));
 	for(int i=0;i<size;i++){
-		data[i] = rand()%10;
+		nums[i] = rand()%10;
 	}
 }
 
-void output(int *data, int size){
-	for (int i = 0; i < size; i++)
-		cout<<data[i]<<" ";
+void Display(int *nums, int size){
+	for(int i = 0;i<size;i++)
+		cout<<nums[i]<<" ";
   cout<<endl;
 }
 
 struct Funnel {
-	Funnel* lr[2];
+	Funnel* ptr[2];
 	void *out;
 	void *in;
 	int size;
@@ -37,7 +35,7 @@ struct Funnel {
 };
 
 
-Funnel* Funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp){
+Funnel* FunnelCreate(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp){
   Funnel *funnel = (Funnel*)malloc(sizeof(funnel));
 	funnel->in = in;
 	funnel->out = out;
@@ -46,74 +44,71 @@ Funnel* Funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp)
 	funnel->pos = 0;
 	funnel->cmp = cmp;
 
-  if (size * nmemb > CACHELINE_SIZE) {
+  if(size*nmemb > CACHELINE_SIZE){
 		size_t nmemb_left = nmemb/2;
 		size_t nmemb_right = nmemb - nmemb_left;
-		void *out = malloc(nmemb * size);
-		funnel->lr[0] = Funnel_create(funnel->in, out, nmemb_left,
-					      size, cmp);
-		funnel->lr[1] = Funnel_create((char*)funnel->in + nmemb_left * size,
-					      (char*)out + nmemb_left * size,
-					      nmemb_right, size, cmp);
+		void *out = malloc(nmemb*size);
+		funnel->ptr[0] = FunnelCreate(funnel->in, out, nmemb_left, size, cmp);
+		funnel->ptr[1] = FunnelCreate((char*)funnel->in + nmemb_left*size,(char*)out + nmemb_left*size, nmemb_right, size, cmp);
 	} else {
-		funnel->lr[0] = funnel->lr[1] = NULL;
+		funnel->ptr[0] = funnel->ptr[1] = NULL;
 	}
 	return funnel;
 }
 
-void* Funnel_pop(Funnel *funnel){
+void* FunnelPop(Funnel *funnel){
 	if (funnel->pos >= funnel->nmemb)
 		return NULL;
-	return (char*)funnel->out + funnel->pos * funnel->size;
+	return (char*)funnel->out + funnel->pos*funnel->size;
   funnel->pos++;
 }
 
-void Funnel_fill(Funnel *funnel){
-	if (funnel->lr[0] == NULL) {
+void FunnelFill(Funnel *funnel){
+	if (funnel->ptr[0] == NULL) {
 		qsort(funnel->in, funnel->nmemb, funnel->size, funnel->cmp);
 		memcpy(funnel->out, funnel->in, funnel->nmemb *funnel->size);
 		return;
 	}
 
-	Funnel_fill(funnel->lr[0]);
-	Funnel_fill(funnel->lr[1]);
+	FunnelFill(funnel->ptr[0]);
+	FunnelFill(funnel->ptr[1]);
 
-	void *lr[2] = { NULL, NULL };
-	int res = 0, i = 0;
+	void *ptr[2] = {NULL,NULL};
+	int res = 0,i = 0;
 	int size = funnel->size;
 
-	lr[res] = Funnel_pop(funnel->lr[res]);
-	assert(lr[res]);
+	ptr[res] = FunnelPop(funnel->ptr[res]);
+	assert(ptr[res]);
 	res = !res;
 	while (i < funnel->nmemb) {
-		lr[res] = Funnel_pop(funnel->lr[res]);
-		if (lr[res] == NULL) {
+		ptr[res] = FunnelPop(funnel->ptr[res]);
+		if (ptr[res] == NULL) {
 			res = !res;
 			do {
-				memcpy((char*)funnel->out + size * i++, lr[res], size);
-				lr[res] = Funnel_pop(funnel->lr[res]);
-			} while (lr[res]);
+				memcpy((char*)funnel->out + size*i++, ptr[res], size);
+				ptr[res] = FunnelPop(funnel->ptr[res]);
+			} while (ptr[res]);
 			assert(i == funnel->nmemb);
 			return;
 		}
-		res = funnel->cmp(lr[0], lr[1]) > 0 ? 1 : 0;
-		memcpy((char*)funnel->out + size * i++, lr[res], size);
-		lr[res] = NULL;
+		res = funnel->cmp(ptr[0], ptr[1]) > 0 ? 1 : 0;
+		memcpy((char*)funnel->out + size*i++, ptr[res], size);
+		ptr[res] = NULL;
 	}
 }
 
-void Funnelsort(void *ptr, int nmemb, size_t size, cmp_t cmp){
-	void *out = malloc(size * nmemb);
-	Funnel* funnel = Funnel_create(ptr, out, nmemb, size, cmp);
-	Funnel_fill(funnel);
-	memcpy(ptr, out, size * nmemb);
+void Funnelsort(void *nums_address, int nmemb, size_t size, cmp_t cmp){
+	void *out = malloc(size*nmemb);
+	Funnel* funnel = FunnelCreate(nums_address, out, nmemb, size, cmp);
+	FunnelFill(funnel);
+	memcpy(nums_address, out, size*nmemb);
 }
 
 int main(){
-  generate(nums,SIZE);
-  output(nums,SIZE);
+  GenerateRandomNumers(nums,SIZE);
+  Display(nums,SIZE);
   Funnelsort(nums,SIZE,sizeof(nums[0]),intcmp);
   cout<<"SORTED ORDER:\n";
-  output(nums,SIZE);
+  Display(nums,SIZE);
   return 0;
 }
