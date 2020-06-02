5b7af62f925b9fc1972dab10a6c945731f78db3c
GARG <garg@GARGs-MacBook-Air.local>
2020-06-02 21:59:36 +0530

    Random input for matrix transpose

--- Non-Cache-Oblivious/randomInputTranspose.cpp
+++ Non-Cache-Oblivious/randomInputTranspose.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ofstream fout;
     fout.open("inputMatrixTranspose.txt");
-    int n = 8192, m = 8192;
+    int n = 64, m = 64;
     fout<<n<<" "<<m<<endl;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
